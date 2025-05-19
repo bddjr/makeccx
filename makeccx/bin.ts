64b0f7f30104529db176e23e6f1af314fed8dfef
bddjr <bddjr08@outlook.com>
2025-05-19 23:22:45 +0800

    create.ts初具雏形

--- makeccx/bin.ts
+++ makeccx/bin.ts
@@ -1,7 +1,7 @@
 if (process.argv[2] === 'build') {
     await import('./builder.js')
 } else if (process.argv[2] === 'create') {
-
+    await import('./create.js')
 } else {
     console.log('unknown command')
     process.exit(1)
