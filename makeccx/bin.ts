be3adf1772fd220d7256e0c0d439fef0e2d397d4
bddjr <bddjr08@outlook.com>
2025-05-20 19:51:44 +0800

    update bin.ts

--- makeccx/bin.ts
+++ makeccx/bin.ts
@@ -3,6 +3,6 @@
 if (process.argv[2] === 'build') {
     await import('./builder.js')
 } else {
-    console.log('makeccx: unknown command')
+    console.log('makeccx: unknown command, please try "npx makeccx build"')
     process.exit(1)
 }
